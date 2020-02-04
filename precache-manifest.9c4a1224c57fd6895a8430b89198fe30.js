4b52bf199a34165f449bb2e3d1ad05afbc7e656b
degrecci <henrique.degrecci@upx.com>
2020-02-04 20:02:08 -0300

    Updates

---
+++ precache-manifest.9c4a1224c57fd6895a8430b89198fe30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652010a6f97b8acf2f9ae13c6e27d405",
+    "url": "/ifood-frontend-test/index.html"
+  },
+  {
+    "revision": "1d440a85b1dae1e2d540",
+    "url": "/ifood-frontend-test/static/css/main.0f7cd881.chunk.css"
+  },
+  {
+    "revision": "600d1327ae9f03c6af14",
+    "url": "/ifood-frontend-test/static/js/2.1ba26619.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/ifood-frontend-test/static/js/2.1ba26619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d440a85b1dae1e2d540",
+    "url": "/ifood-frontend-test/static/js/main.43bb6d35.chunk.js"
+  },
+  {
+    "revision": "3c6d5e4c8ea467f4877e",
+    "url": "/ifood-frontend-test/static/js/runtime-main.0815624d.js"
+  }
+]);
